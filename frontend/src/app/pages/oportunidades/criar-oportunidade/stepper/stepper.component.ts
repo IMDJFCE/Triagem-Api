1baf37d8c982bdbbc85d594ab0ea8ed5cea0d56e
David  Willian Pereira  Jatoba <r_david.jatoba@jfce.jus.br>
2024-06-21 10:20:28 -0300

    adicionando chips para as habilidades tecnicas da oportunidade

--- frontend/src/app/pages/oportunidades/criar-oportunidade/stepper/stepper.component.ts
+++ frontend/src/app/pages/oportunidades/criar-oportunidade/stepper/stepper.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
-import { SkillsService } from 'src/app/services/skills/skills.service';
 import { OportunidadeService } from 'src/app/services/oportunidade/oportunidade.service';
 import { OportunidadeRequest } from 'src/app/models/OportunidadeRequest';
 import { Router } from '@angular/router';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { HabilidadeRequest } from 'src/app/models/HabilidadeRequest';
+import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import { HabilidadeTipo } from 'src/app/models/HabilidadeTipo';
 
 @Component({
   selector: 'app-stepper',
@@ -19,6 +23,13 @@ import { Router } from '@angular/router';
 })
 
 export class StepperComponent {
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  skills: HabilidadeRequest[] = [{nome: 'Lógica', tipo: HabilidadeTipo.TECNICA}, {nome: 'Testes Unitários', tipo: HabilidadeTipo.TECNICA}];
+  habilidadesSelecionadas: HabilidadeRequest[] = [];
+
+  announcer = inject(LiveAnnouncer);
+
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
     startDateControl: ['', Validators.required],
@@ -34,23 +45,20 @@ export class StepperComponent {
 
   isLinear = false;
 
-  constructor(private _formBuilder: FormBuilder, private skillsService: SkillsService, private oportunidadeService: OportunidadeService, private router: Router){}
+  constructor(private _formBuilder: FormBuilder, private oportunidadeService: OportunidadeService, private router: Router){}
 
   finalizar(){
     const firstFormGroupValues = this.firstFormGroup.value;
+    this.adicionarHabilidadesTecnicasSelecionadas();
 
     const oportunidadeRequest: OportunidadeRequest = {
       titulo: firstFormGroupValues.firstCtrl ?? '',
       dataInicial: this.parseDate(firstFormGroupValues.startDateControl),
       dataFinal: this.parseDate(firstFormGroupValues.endDateControl),
       descricao: firstFormGroupValues.textControl ?? '',
-      habilidades: [],
+      habilidades: this.habilidadesSelecionadas,
       email: firstFormGroupValues.emailControl ?? ''
     }
-    
-    this.skillsService.skills$.subscribe(skills => {
-      oportunidadeRequest.habilidades = skills;
-    });
     
     this.oportunidadeService.createOportunidade(oportunidadeRequest).subscribe({
       next: (response) => {
@@ -65,10 +73,6 @@ export class StepperComponent {
     });
   }
 
-  ngOnDestroy() {
-    this.skillsService.clearSkills();
-  }
-
   parseDate(value: string | Date | null | undefined):Date {
     if (value instanceof Date) {
       return value; 
@@ -76,6 +80,54 @@ export class StepperComponent {
       return new Date(value.replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$3-$1-$2'));
     } else {
       return new Date(); 
+    }
+  }
+
+  adicionarHabilidadesTecnicasSelecionadas(): void {
+    for (let skill of this.skills) {
+      this.habilidadesSelecionadas.push(skill);
+    }
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our skill
+    if (value) {
+      const newSkill: HabilidadeRequest = {
+        nome: value,
+        tipo: HabilidadeTipo.TECNICA
+      };
+      this.skills.push(newSkill);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(skill: HabilidadeRequest): void {
+    const index = this.skills.indexOf(skill);
+
+    if (index >= 0) {
+      this.skills.splice(index, 1);
+
+      this.announcer.announce(`Removed ${skill}`);
+    }
+  }
+
+  edit(skill: HabilidadeRequest, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove skill if it no longer has a name
+    if (!value) {
+      this.remove(skill);
+      return;
+    }
+
+    // Edit existing skill
+    const index = this.skills.indexOf(skill);
+    if (index >= 0) {
+      this.skills[index].nome = value;
     }
   }
 }
