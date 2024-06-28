3beca4843ef2f453e49c1f15ec9504445da0e971
Allan Pablo <allanpablo@gmail.com>
2024-06-27 23:08:12 -0300

    Add files via upload

--- frontend/src/app/pages/triagem/modal/modal.component.ts
+++ frontend/src/app/pages/triagem/modal/modal.component.ts
@@ -1,6 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  inject,
+} from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
-import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+} from "@angular/material/dialog";
 
 @Component({
   selector: "app-modal",
@@ -13,19 +22,28 @@ import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 export class ModalComponent {
   readonly dialog = inject(MatDialog);
 
-  openDialog() {
-    const dialogRef = this.dialog.open(modalContent);
+  openDialog(data: any) {
+    const dialogRef = this.dialog.open(ModalContent, {
+      data: data,
+    });
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
 }
+
 @Component({
-  selector: "modalComponent-content",
-  templateUrl: "modal-dialog.component.html",
+  selector: "modal-content",
+  templateUrl: "./modal-dialog.component.html",
   standalone: true,
   imports: [MatDialogModule, MatButtonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class modalContent {}
+export class ModalContent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  get usuario() {
+    return this.data.usuario;
+  }
+}
